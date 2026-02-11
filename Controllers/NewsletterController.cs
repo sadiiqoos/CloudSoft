3a70e38fdb8db845a846b47320089021bd82e03f
sadiiqoos <sadiiqoos@yahoo.com>
2026-02-11 20:54:25 +0100

    new uppdate

--- Controllers/NewsletterController.cs
+++ Controllers/NewsletterController.cs
@@ -1,3 +1,4 @@
+using CloudSoft.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudSoft.Controllers;
@@ -13,15 +14,16 @@ public class NewsletterController : Controller
 
     // POST: /Newsletter/Subscribe
     [HttpPost]
-    public IActionResult Subscribe(string name, string email)
+    public IActionResult Subscribe(Subscriber subscriber)
     {
         // Add subscription logic here
         // ...
 
         // Write to the console
-        Console.WriteLine($"New subscription - Name: {name} Email: {email}");
+        string value = $"New subscription - Name: {subscriber.Name} Email: {subscriber.Email}";
+        Console.WriteLine(value: value);
 
         // Send a message to the user
-        return Content($"Thank you {name} for subscribing to our newsletter!");
+        return Content($"Thank you {subscriber.Name} for subscribing to our newsletter!");
     }
 }
---
+++ Models/Subscribe.cs
@@ -0,0 +1,7 @@
+namespace CloudSoft.Models;
+
+public class Subscriber
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
